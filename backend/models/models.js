87476d741652cc776d76ffe2de9c21c389263873
TrevonCdr <89557721+TrevonCdr@users.noreply.github.com>
2022-10-05 14:52:42 -0500

    Add files via upload

---
+++ backend/models/models.js
@@ -0,0 +1,114 @@
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//collection for intakeData
+let primaryDataSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    firstName: {
+        type: String,
+        require: true
+    },
+    middleName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    phoneNumbers: {
+        type: Array,
+        required: true
+    },
+    address: {
+        line1: {
+            type: String
+        },
+        line2: {
+            type: String,
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        county: {
+            type: String,
+        },
+        zip: {
+            type: String,
+        }
+    },
+    organizationName: {
+       type: String
+    }    
+}, {
+    collection: 'primaryData',
+    timestamps: true
+});
+
+//collection for eventData
+let eventDataSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    eventName: {
+        type: String,
+        require: true
+    },
+    services: {
+        type: Array
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    address: {
+        line1: {
+            type: String
+        },
+        line2: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        county: {
+            type: String,
+        },
+        zip: {
+            type: String,
+        }
+    },
+    description: {
+        type: String,
+    },
+    attendees: [{
+        type: String
+    }],
+    organizationName: {
+        type: String
+    }    
+}, {
+    collection: 'eventData'
+});
+
+// collection for organziationData
+let organizationDataSchema = new Schema({
+    organizationId: { type: Number, required: true, unique: true },
+    name: {
+        type: String,
+        require: true
+    }
+}, {
+    collection: 'organizationData',
+    timestamps: true
+});
+
+// create models from mongoose schemas
+const primarydata = mongoose.model('primaryData', primaryDataSchema);
+const eventdata = mongoose.model('eventData', eventDataSchema);
+const organizationdata = mongoose.model('organizationData', organizationDataSchema);
+
+// package the models in an object to export 
+module.exports = { primarydata, eventdata, organizationdata }
