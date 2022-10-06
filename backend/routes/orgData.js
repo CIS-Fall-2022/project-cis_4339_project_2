a71baeef7942fd9a05f7b4368df3d485169804eb
jareyesc <70295804+jareyesc@users.noreply.github.com>
2022-10-06 00:22:03 -0500

    Uploaded Organization Schema
    In my orgData file, I upgraded the search query which went from asking for the orgname and email in the parameters to just only the orgname. I also included the events for a single organization API and created a clients for a single organization API.

---
+++ backend/routes/orgData.js
@@ -0,0 +1,134 @@
+const express = require("express"); 
+const router = express.Router(); 
+
+//importing data model schemas
+let { orgData } = require("../models/models"); 
+let { eventdata } = require("../models/models"); 
+let { primarydata } = require("../models/models");
+
+//GET all entries
+router.get("/", (req, res, next) => { 
+    orgData.find( 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
+//GET single entry by ID
+router.get("/id/:id", (req, res, next) => {
+    orgData.find( 
+        { _id: req.params.id }, 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//GET entries based on search query
+//Ex: '...?firstName=Bob&lastName=&searchBy=name' 
+router.get("/search/", (req, res, next) => { 
+    let dbQuery = "";
+    if (req.query["searchBy"] === 'name') {
+        dbQuery = { orgname: { $regex: `^${req.query["orgname"]}`, $options: "i" } }
+    } else if (req.query["searchBy"] === 'email') {
+        dbQuery = {
+            email:  req.query["orgEmail"]
+        }
+    };
+    orgData.find( 
+        dbQuery, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//GET events for a single org
+router.get("/event/:orgName", (req, res, next) => { 
+    eventdata.find( 
+        { organization: req.params.orgName }, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//GET clients for a single org
+router.get("/client/:orgName", (req, res, next) => { 
+    primarydata.find( 
+        { organization: req.params.orgName }, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//POST
+router.post("/", (req, res, next) => { 
+    orgData.create( 
+        req.body,
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data); 
+            }
+        }
+    );
+    orgData.createdAt;
+    orgData.updatedAt;
+    orgData.createdAt instanceof Date;
+});
+
+//PUT update (make sure req body doesn't have the id)
+router.put("/:id", (req, res, next) => { 
+    orgData.findOneAndUpdate( 
+        { _id: req.params.id }, 
+        req.body,
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+router.delete('/:id', (req, res, next) => {
+    
+    orgData.findOneAndRemove({ _id: req.params.id}, 
+        req.body,
+        (error, data) => {
+        if (error) {
+          return next(error);
+        } else {
+           res.status(200).json({
+             msg: data
+           });
+        }
+      });
+});
+
+module.exports = router;
