cee93c96d907bfa6b8915cf423141054a275ef56
jareyesc <70295804+jareyesc@users.noreply.github.com>
2022-10-06 23:16:13 -0500

    Update app.js
    This is our final commit of app.js for Sprint 2.

--- backend/app.js
+++ backend/app.js
@@ -33,10 +33,11 @@ app.use(morgan("dev"));
 //import routes
 const primaryDataRoute  = require('./routes/primaryData');
 const eventsDataRoute  = require('./routes/eventsData');
-
+const orgDataRoute  = require('./routes/orgData');
 //setup middle ware for routes
 app.use('/primaryData', primaryDataRoute);
-app.use('/eventData', eventsDataRoute)
+app.use('/eventData', eventsDataRoute);
+app.use('/orgData', orgDataRoute);
 
 app.listen(PORT, () => {
   console.log("Server started listening on port : ", PORT);
@@ -44,6 +45,9 @@ app.listen(PORT, () => {
 
 //error handler
 app.use(function (err, req, res, next) {
+  if (!err) {
+    return next();
+  }
   // logs error and error code to console
   console.error(err.message, req);
   if (!err.statusCode)
